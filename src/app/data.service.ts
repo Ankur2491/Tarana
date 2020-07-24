ed1e88e16282887a8e7e27a7ccf13d42debe5d5d
ankur2491 <ankur.101045@gmail.com>
2020-07-25 03:23:02 +0530

    First Commit

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,29 +1,22 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-
   private urlSource = new BehaviorSubject('default url');
   currentUrl = this.urlSource.asObservable();
   radioObject = {
     "Vybez station": "http://us2.internet-radio.com:8281/live"
     ,
-
     "Radio Retro": "http://64.71.79.181:5124/;stream/1"
     ,
-
     "Hits Of Bollywood": "http://50.7.77.115:8174/;"
     ,
-
     "Non Stop Hindi": "http://198.178.123.14:8216/;stream/1"
     ,
-
     "Shripad Radio": "http://144.217.203.184:8112/;stream/1"
     ,
-
     "Sada Bahar Bollywood": "http://192.240.102.133:8512/;stream/1"
     ,
     "Desi Music Mix": "http://66.23.234.242:8012/;stream/1"
@@ -32,12 +25,10 @@ export class DataService {
     ,
     "Masala Radio": "http://198.178.123.23:7354/;stream/1"
     ,
-
     "Arijit Singh": "http://212.83.138.48:8740/;stream/1"
     ,
     "AR Rahman": "http://212.83.138.48:8708/;stream/1"
     ,
-
     "DesiZone Radio": "http://www.desizone.nl:8000/;"
     ,
     "RadioCity": "http://prclive1.listenon.in:9960/"
@@ -62,13 +53,13 @@ export class DataService {
     ,
     "Radio Dil": "http://50.22.253.45/radiodil"
     ,
-    "City 1016": "http://19393.live.streamtheworld.com:3690/ARNCITYAAC_SC?DIST=TuneIn&TGT=TuneIn&maxServers=2&gender=m&ua=RadioTime&ttag=RadioTime"
+    "City 1016": "http://19393.live.streamtheworld.com:3690/ARNCITYAAC_SC?DIST=TuneIn&TGT=TuneIn&maxServers=2&gender=m & ua=RadioTime& ttag=RadioTime"
     ,
     "Evergreen Hits": "http://192.240.102.133:11454/stream"
     ,
     "Hot 89.9": "http://newcap.leanstream.co/CIHTFM?args=tunein_02"
     ,
-    "Go 96.3": "http://18863.live.streamtheworld.com:3690/KTWNFM_SC?DIST=TuneIn&TGT=TuneIn&maxServers=2&gender=m&ua=RadioTime&ttag=RadioTime"
+    "Go 96.3": "http://18863.live.streamtheworld.com:3690/KTWNFM_SC?DIST=TuneIn&TGT=TuneIn&maxServers=2&gender=m&ua= RadioTime & ttag=RadioTime"
     ,
     "Hits 99.9": "http://17263.live.streamtheworld.com:3690/KIOAHD2_SC?DIST=TuneIn&TGT=TuneIn&maxServers=2&tdtok=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImtpZCI6ImZTeXA4In0.eyJpc3MiOiJ0aXNydiIsInN1YiI6I"
     ,
@@ -100,7 +91,7 @@ export class DataService {
     ,
     "Red FM": "http://14013.live.streamtheworld.com/CKYRFM_SC"
     ,
-    "Big FM": "http://sc-bb.1.fm:8017/"
+    "Big FM": "https://radioindia.net/radio/sc-bb/icecast.audio"
     ,
     "Fresh Bollywood": "http://103.16.47.70:7222/;stream.nsv"
     ,
@@ -110,31 +101,26 @@ export class DataService {
     ,
     "Mohd. Rafi": "http://prclive1.listenon.in:8814/"
     ,
-    "FM Gold": "http://51.15.208.163:8081/radio/fmgold/icecast.audio"
+    "FM Gold": "https://onlineradiofm.in/apii.php?url=https://radioindia.net/radio/fm_gold/icecast.audio"
     ,
-    "FM Rainbow": "http://51.15.208.163:8081/radio/fmrainbow/icecast.audio"
+    "FM Rainbow": "https://onlineradiofm.in/apii.php?url=https://radioindia.net/radio/rainbow_radio/icecast.audio"
     ,
-    "Vividh Bharti": "http://51.15.208.163:8081/radio/vividhbharati/icecast.audio"
+    "Vividh Bharti": "https://onlineradiofm.in/apii.php?url=https://radioindia.net/radio/vividhbharti/icecast.audio"
     ,
     "Asha": "http://prclive1.listenon.in:8812/;"
     ,
     "Talat Mahmood": "http://ec2-34-217-86-229.us-west-2.compute.amazonaws.com:9600/talat.ogg"
     ,
-    "Ustad Ahmed & Mohammad Hussain": "http://ec2-34-217-86-229.us-west-2.compute.amazonaws.com:9601/hussain.ogg",
-    
-    "Thumri": "http://ec2-54-244-2-94.us-west-2.compute.amazonaws.com:9600/thumri.ogg"
- }
+    "Ustad Ahmed & Mohammad Hussain": "http://ec2-34-217-86-229.us-west-2.compute.amazonaws.com:9601/hussain.ogg"
+  }
     ;
-
   private nameSource = new BehaviorSubject('');
   currentName = this.nameSource.asObservable();
-
   constructor() { }
   changeTheUrl(url: string) {
     let au = this.radioObject[url];
     this.urlSource.next(au);
   }
-
   changeControl(name: string) {
     this.nameSource.next(name);
   }
